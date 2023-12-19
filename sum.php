0fd619b27b5d0eace5d6204af6a4283966d32dd5
Jérôme Perriguey <jerome.perriguey@inrae.fr>
2023-12-19 15:06:39 +0100

    add function divide in sum.php

--- sum.php
+++ sum.php
@@ -5,9 +5,14 @@ function sum($value1, $value2)
     return $value1 + $value2;
 }
 
+
 function multiply($value1, $value2) 
 {
     return $value1 * $value2;
 }
 
 function divide($value1, $value2)
+{
+   return $value1 / $value2;
+}
+
